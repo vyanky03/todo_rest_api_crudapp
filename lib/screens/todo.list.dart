45dbb817f7a8f108b7d4bebde88d50e422d10bd1
Vyankatesh Kshatriya <vyankatesh.kshatriya@gmail.com>
2023-11-30 15:53:42 +0530

    update

---
+++ lib/screens/todo.list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodooListPage extends StatefulWidget {
+  const TodooListPage({super.key});
+
+  @override
+  State<TodooListPage> createState() => _TodooListPageState();
+}
+
+class _TodooListPageState extends State<TodooListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
